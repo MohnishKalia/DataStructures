221aff889ce9e09073718169d40a23f3fd2e5a0e
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-08-25 22:01:24 -0500

    Assignment 1 POC
    Using a throws Exception, wait till class starts to change

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -1,9 +1,38 @@
 package assignment1;
 
+import java.util.*;
+import java.io.*;
+
 public class EmailMerge {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		/*
+		 * 1 line of people.txt includes a person's info Store in string[] and iterate
+		 * {Name} {Age} {Gender} As a result, .split(" ") and 0=N, 1=A, 2=G Then send
+		 * message using message template and .replaceAll
+		 */
+
+		ArrayList<String> persons = new ArrayList<String>();
+		String message = "";
+		Scanner template = new Scanner(new File("src/assignment1/template.txt"));
+		Scanner people = new Scanner(new File("src/assignment1/people.txt"));
+
+		while (template.hasNextLine())
+			message += template.nextLine();
+		System.out.println(message);
+
+		while (people.hasNextLine())
+			persons.add(people.nextLine());
+
+		for (String person : persons) {
+			String[] info = person.split(" ");
+			String temp = message;
+			temp = temp.replaceAll("<<N>>", info[0]);
+			temp = temp.replaceAll("<<A>>", info[1]);
+			temp = temp.replaceAll("<<G>>", info[2]);
+			System.out.println(temp);
+		}
 
 	}
 
