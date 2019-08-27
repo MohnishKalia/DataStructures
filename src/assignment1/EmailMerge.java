18a4e461ab5646e48be0509c27c65a1c61f9337b
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-08-27 00:30:10 -0500

    Correct alternate path

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -2,38 +2,28 @@ package assignment1;
 
 import java.util.*;
 import java.io.*;
+import java.nio.file.*;
 
 public class EmailMerge {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		/*
-		 * 1 line of people.txt includes a person's info Store in string[] and iterate
-		 * {Name} {Age} {Gender} As a result, .split(" ") and 0=N, 1=A, 2=G Then send
-		 * message using message template and .replaceAll
-		 */
-
-		ArrayList<String> persons = new ArrayList<String>();
-		String message = "";
-		Scanner template = new Scanner(new File("src/assignment1/template.txt"));
-		Scanner people = new Scanner(new File("src/assignment1/people.txt"));
-
-		while (template.hasNextLine())
-			message += template.nextLine();
-		System.out.println(message);
-
-		while (people.hasNextLine())
-			persons.add(people.nextLine());
-
-		for (String person : persons) {
+		List<String> template = Files.readAllLines(Paths.get("src/assignment1/template.txt"));
+		List<String> people = Files.readAllLines(Paths.get("src/assignment1/people.txt"));
+		for (String person : people) {
 			String[] info = person.split(" ");
-			String temp = message;
-			temp = temp.replaceAll("<<N>>", info[0]);
-			temp = temp.replaceAll("<<A>>", info[1]);
-			temp = temp.replaceAll("<<G>>", info[2]);
-			System.out.println(temp);
+			List<String> email = new ArrayList<>(template);			
+			for (ListIterator<String> i = email.listIterator(); i.hasNext();)
+				i.set(i.next().replaceAll("<<N>>", info[0]).replaceAll("<<A>>", info[1]).replaceAll("<<G>>", info[2]));
+			
+			String dir = "src/assignment1/%s";
+			Path path = Paths.get(String.format(dir + ".txt", info[0]));
+			if (Files.exists(path))
+				for (int i = 1; Files.exists(path); i++)
+					path = Paths.get(String.format(dir + "-" + i + ".txt", info[0]));
+			
+			Files.write(path, email);
 		}
-
 	}
 
 }
