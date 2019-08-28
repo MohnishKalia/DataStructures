000a64682b79ac5232e4f7261fdefe78a49ed201
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-08-28 11:40:10 -0500

    Removed unused imports

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -6,9 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 public class EmailMerge {
