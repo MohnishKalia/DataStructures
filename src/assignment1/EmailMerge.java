03e6c82e37caa56750693d26fbc601130caa5988
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-08-28 01:08:07 -0500

    Streams version

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 public class EmailMerge {
 
@@ -18,16 +19,16 @@ public class EmailMerge {
 		List<String> people = Files.readAllLines(Paths.get("src/assignment1/people.txt"));
 		for (String person : people) {
 			String[] info = person.split(" ");
-			List<String> email = new ArrayList<>(template);			
-			for (ListIterator<String> i = email.listIterator(); i.hasNext();)
-				i.set(i.next().replaceAll("<<N>>", info[0]).replaceAll("<<A>>", info[1]).replaceAll("<<G>>", info[2]));
-			
+			List<String> email = template.stream().map(
+					line -> line.replaceAll("<<N>>", info[0]).replaceAll("<<A>>", info[1]).replaceAll("<<G>>", info[2]))
+					.collect(Collectors.toList());
+
 			String dir = "src/assignment1/%s";
 			Path path = Paths.get(String.format(dir + ".txt", info[0]));
 			if (Files.exists(path))
 				for (int i = 1; Files.exists(path); i++)
 					path = Paths.get(String.format(dir + "-" + i + ".txt", info[0]));
-			
+
 			Files.write(path, email);
 		}
 	}
