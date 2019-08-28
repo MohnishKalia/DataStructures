e0ebe1edd9494c13c9d992af8753fdb1f8241625
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-08-28 12:46:49 -0500

    Prod. version with files in root directory

--- src/assignment1/EmailMerge.java
+++ src/assignment1/EmailMerge.java
@@ -1,5 +1,4 @@
 package assignment1;
-
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -15,8 +14,8 @@ public class EmailMerge {
 	
 	public static void main(String[] args) throws IOException {
 		// Transferring lines of text into entries in a list (unmodifiable list)
-		List<String> template = Files.readAllLines(Paths.get("src/assignment1/template.txt"));
-		List<String> people = Files.readAllLines(Paths.get("src/assignment1/people.txt"));
+		List<String> template = Files.readAllLines(Paths.get("template.txt"));
+		List<String> people = Files.readAllLines(Paths.get("people.txt"));
 
 		for (String person : people) {
 			String[] info = person.split(" "); // splits info string into [name, age, gender]
@@ -30,7 +29,7 @@ public class EmailMerge {
 					.replaceAll("<<G>>", info[2]))
 					.collect(Collectors.toList());
 
-			String dir = "src/assignment1/%s%s.txt"; // template format string for filename
+			String dir = "%s%s.txt"; // template format string for filename
 			Path path = Paths.get(String.format(dir, info[0], "")); // generates path with just the name (ex."John.txt")
 
 			// loop to check if there are any file collisions, replacing the standard path
