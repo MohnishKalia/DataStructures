7661a671e25b028e14e2117e11ff3cdd19ae1a5b
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-09-11 10:58:27 -0500

    Implemented algorithm

--- src/assignment2/PrefixAverageCalculations.java
+++ src/assignment2/PrefixAverageCalculations.java
@@ -1,14 +1,22 @@
 package assignment2;
 
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * This application compares the time complexities for two Prefix Average
- * calulation algorithms and exports the data to a times.csv file within the
+ * calculation algorithms and exports the data to a times.csv file within the
  * same directory level.
- * 
+ *
  * @author Mohnish Kalia
  */
 public class PrefixAverageCalculations {
@@ -20,7 +28,7 @@ public class PrefixAverageCalculations {
             boolean flag = true;
             while (flag) {
                 System.out.println("What is the input size you wish to test?");
-                long[] data = computeTimes(input.nextLong());
+                long[] data = computeTimes(input.nextInt());
                 dataLines.add(String.format(dataFormat, data[0], data[1], data[2]));
                 System.out.println("Continue? y : n");
                 input.nextLine();
@@ -32,28 +40,63 @@ public class PrefixAverageCalculations {
 
     /**
      * Computes the time of the processed algorithms and return the data
-     * 
+     *
      * @param inputSize the desired value of inputs
-     * @return a long array with 3 values: the input size, time for algorithm 1, and the time
-     *         for algorithm 2
+     * @return a long array with 3 values: the input size, time for algorithm 1, and
+     *         the time for algorithm 2
      */
-    private static long[] computeTimes(long inputSize) throws InterruptedException {
+    private static long[] computeTimes(int inputSize) throws InterruptedException {
+        double[] values = new double[inputSize];
+        for (int i = 0; i < values.length; i++)
+            values[i] = Math.random() * 50;
+
         long[] data = new long[3];
         long before, after;
         data[0] = inputSize;
 
         before = System.currentTimeMillis();
         // run alg 1
-        Thread.sleep(inputSize);
+        prefixAverage1(values);
         after = System.currentTimeMillis();
         data[1] = after - before;
 
         before = System.currentTimeMillis();
         // run alg 2
-        Thread.sleep(inputSize * 3);
+        prefixAverage2(values);
         after = System.currentTimeMillis();
         data[2] = after - before;
 
         return data;
+    }
+
+    /**
+     * Returns an array a such that, for all j, a[j] equals the average of x[0],
+     * ..., x[j].
+     */
+    public static double[] prefixAverage1(double[] x) {
+        int n = x.length;
+        double[] a = new double[n]; // filled with zeros by default
+        for (int j = 0; j < n; j++) {
+            double total = 0; // begin computing x[0] + ... + x[j]
+            for (int i = 0; i <= j; i++)
+                total += x[i];
+            a[j] = total / (j + 1); // record the average
+        }
+        return a;
+    }
+
+    /**
+     * Returns an array a such that, for all j, a[j] equals the average of x[0],
+     * ..., x[j].
+     */
+    public static double[] prefixAverage2(double[] x) {
+        int n = x.length;
+        double[] a = new double[n]; // filled with zeros by default
+        double total = 0; // compute prefix sum as x[0] + x[1] + ...
+        for (int j = 0; j < n; j++) {
+            total += x[j]; // update prefix sum to include x[j]
+            a[j] = total / (j + 1); // compute average based on current sum
+        }
+        return a;
     }
 }
