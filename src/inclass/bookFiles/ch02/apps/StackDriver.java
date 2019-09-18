9fb25030c64fce11f865b8697aafa4051c4c2fb3
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-09-18 12:46:44 -0500

    Test program for stack driver

--- src/inclass/bookFiles/ch02/apps/StackDriver.java
+++ src/inclass/bookFiles/ch02/apps/StackDriver.java
@@ -1,11 +1,19 @@
-package ch02.apps;
-import ch02.stacks.*;
+package inclass.bookFiles.ch02.apps;
 
-public class StackDriver 
-{
-  public static void main(String[] args)
-  {
-    StackInterface<String> test;
-    test = new ArrayBoundedStack01<String>();    
+import inclass.bookFiles.ch02.stacks.ArrayBoundedStack;
+
+public class StackDriver {
+  public static void main(String[] args) {
+    ArrayBoundedStack<String> test = new ArrayBoundedStack<String>();
+    String[] elements = "This is a test of the stack data structure".split(" "); // tokenizes each word
+    for (String elt : elements)
+      test.push(elt);
+    System.out.println(test.top()); // expected "structure"
+    // test.remove(10); // should throw IllegalArgumentExeception
+    test.remove(2);
+    System.out.println(test.top()); // expected "stack"
+    test.clear();
+    // System.out.println(test.top()); // should throw StackUnderflowException
+    // test.clear(); // should throw StackUnderflowException
   }
 }
--- src/inclass/bookFiles/ch02/stacks/ArrayBoundedStack.java
+++ src/inclass/bookFiles/ch02/stacks/ArrayBoundedStack.java
@@ -11,30 +11,26 @@
 
 package inclass.bookFiles.ch02.stacks;
 
-public class ArrayBoundedStack<T> implements StackInterface<T> 
-{
+public class ArrayBoundedStack<T> implements StackInterface<T> {
   protected final int DEFCAP = 100; // default capacity
-  protected T[] elements;           // holds stack elements
-  protected int topIndex = -1;      // index of top element in stack
+  protected T[] elements; // holds stack elements
+  protected int topIndex = -1; // index of top element in stack
 
-  public ArrayBoundedStack() 
-  {
+  public ArrayBoundedStack() {
     elements = (T[]) new Object[DEFCAP];
   }
 
-  public ArrayBoundedStack(int maxSize) 
-  {
+  public ArrayBoundedStack(int maxSize) {
     elements = (T[]) new Object[maxSize];
   }
 
   public void push(T element)
   // Throws StackOverflowException if this stack is full,
   // otherwise places element at the top of this stack.
-  {      
+  {
     if (isFull())
       throw new StackOverflowException("Push attempted on a full stack.");
-    else
-    {
+    else {
       topIndex++;
       elements[topIndex] = element;
     }
@@ -43,11 +39,10 @@ public class ArrayBoundedStack<T> implements StackInterface<T>
   public void pop()
   // Throws StackUnderflowException if this stack is empty,
   // otherwise removes top element from this stack.
-  {                  
+  {
     if (isEmpty())
       throw new StackUnderflowException("Pop attempted on an empty stack.");
-    else
-    {
+    else {
       elements[topIndex] = null;
       topIndex--;
     }
@@ -56,7 +51,7 @@ public class ArrayBoundedStack<T> implements StackInterface<T>
   public T top()
   // Throws StackUnderflowException if this stack is empty,
   // otherwise returns top element of this stack.
-  {                 
+  {
     T topOfStack = null;
     if (isEmpty())
       throw new StackUnderflowException("Top attempted on an empty stack.");
@@ -67,13 +62,37 @@ public class ArrayBoundedStack<T> implements StackInterface<T>
 
   public boolean isEmpty()
   // Returns true if this stack is empty, otherwise returns false.
-  {              
-    return (topIndex == -1); 
+  {
+    return (topIndex == -1);
   }
 
   public boolean isFull()
   // Returns true if this stack is full, otherwise returns false.
-  {              
+  {
     return (topIndex == (elements.length - 1));
+  }
+
+  /**
+   * Pops a specified number of elements from the stack. Time complexity is O(n)
+   * where n is the input (e.g. if removing 5 elements, time comp. is O(5)).
+   * 
+   * @param n number of elements to remove
+   */
+  public void remove(int n) {
+    if (n > topIndex)
+      throw new IllegalArgumentException("Specified index is too large for the current stack size.");
+    for (int i = 0; i < n; i++)
+      pop();
+  }
+
+  /**
+   * Clears all elements within the stack. Time complexity is O(n) where n is the
+   * # of non-null elements.
+   */
+  public void clear() {
+    if (isEmpty())
+      throw new StackUnderflowException("Clear attempted on an empty stack.");
+    while (topIndex > -1)
+      pop();
   }
 }
