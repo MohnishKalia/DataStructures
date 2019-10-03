008d21c5f5667a2b20154c191860d8cc9a972ae5
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-10-03 13:43:17 -0500

    Intermediate zip utility

---
+++ src/testPrograms/ZipFolder.java
@@ -0,0 +1,55 @@
+package testPrograms;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipFolder {
+
+    public static void main(String[] args) throws Exception {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("What is the folder you wish to zip?\n\nAvailable Folders:");
+            for (var s : new File("src").list())
+                System.out.println("    " + s);
+            String folder = input.nextLine();
+            ZipFolder.zipFolder(new File("src/" + folder),
+                    new File(String.format("src/%s/Kalia%s.zip", folder, folder)));
+        }
+    }
+
+    public static void zipFolder(File srcFolder, File destZipFile) throws Exception {
+        try (FileOutputStream fileWriter = new FileOutputStream(destZipFile);
+                ZipOutputStream zip = new ZipOutputStream(fileWriter)) {
+            addFolderToZip(srcFolder, srcFolder, zip);
+        }
+    }
+
+    private static void addFileToZip(File rootPath, File srcFile, ZipOutputStream zip) throws Exception {
+
+        if (srcFile.isDirectory()) {
+            addFolderToZip(rootPath, srcFile, zip);
+        } else {
+            byte[] buf = new byte[1024];
+            int len;
+            try (FileInputStream in = new FileInputStream(srcFile)) {
+                String name = srcFile.getPath();
+                System.out.println("Zip " + srcFile + "\n to " + name);
+                zip.putNextEntry(new ZipEntry(name));
+                while ((len = in.read(buf)) > 0) {
+                    zip.write(buf, 0, len);
+                }
+            }
+        }
+    }
+
+    private static void addFolderToZip(File rootPath, File srcFolder, ZipOutputStream zip) throws Exception {
+        for (File fileName : srcFolder.listFiles()) {
+            addFileToZip(rootPath, fileName, zip);
+        }
+    }
+
+    // should be store the file paths in an list, create a zip file; then for each file, add to the zip. See nio api
+}
