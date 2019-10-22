703a70477079f8351ec4cd40b22a541a8b83fed9
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-10-22 13:11:07 -0500

    Added proper test

--- src/ch05/apps/CollectionTest.java
+++ src/ch05/apps/CollectionTest.java
@@ -1,17 +1,19 @@
 package ch05.apps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Test;
+
 public class CollectionTest {
-    public static void main(String[] args) {
-        System.out.println(
-            removeDuplicates(
-                List.of(
-                    "This is a test test of the duplication of the thing".split(" ")
-                )
-            )
-        );
+
+    @Test
+    public void testRemoveDupilcates() {
+        List<String> words = List.of("This is a test test of the duplication of the thing".split(" "));
+        List<String> expected = List.of("This is a test of the duplication thing".split(" "));
+        assertEquals(expected, removeDuplicates(words));
     }
 
     private static <T> List<T> removeDuplicates(List<T> of) {
